01d7447517793b009c8d9510705ee08dc163495b
Prakharpatel24 <prakharpatel2001@gmail.com>
2024-12-16 07:32:00 +0530

    built feed api and added pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const userRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 require('dotenv').config();
 
 //GET all the pending user requests
@@ -22,7 +23,7 @@ userRouter.get("/connections", userAuth, async (req, res) => {
     }).populate("fromUserId", process.env.USER_SAFE_DATA)
         .populate("toUserId", process.env.USER_SAFE_DATA)
     const response = connections.map((field) => {
-        if(field.fromUserId._id.toString() === loggedInUserId.toString()){
+        if (field.fromUserId._id.toString() === loggedInUserId.toString()) {
             return field.toUserId;
         }
         return field.fromUserId;
@@ -31,5 +32,36 @@ userRouter.get("/connections", userAuth, async (req, res) => {
         message: 'Successfully fetched the connections',
         data: response,
     });
-})
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
+    const loggedInUserId = req.user._id;
+    //all the requests either sent or received by the logged in user 
+    const existingRequests = await ConnectionRequest.find({
+        $or: [
+            { toUserId: loggedInUserId },
+            { fromUserId: loggedInUserId }
+        ]
+    }).select("fromUserId toUserId");
+
+    const hideUsersFromFeed = new Set();
+    existingRequests.forEach((req) => {
+        hideUsersFromFeed.add(req.fromUserId.toString());
+        hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+        _id: { $nin: Array.from(hideUsersFromFeed) }
+    })
+        .select(process.env.USER_SAFE_DATA)
+        .skip(skip)
+        .limit(limit);
+
+    res.send(users);
+});
 module.exports = userRouter;
