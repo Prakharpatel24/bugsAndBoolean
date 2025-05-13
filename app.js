ee6ce28dfc40658299b5976d10673f22aa6b004d
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-14 00:17:36 +0530

    added_middleware_for_handling_cors

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const authRouter = require("./src/routes/auth");
 const profileRouter = require("./src/routes/profile");
 const requestRouter = require("./src/routes/request");
 const userRouter = require("./src/routes/user");
+const cors = require("cors");
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
 
 //using the middleware provided to us by Express for converting json objects to js objects
 app.use(express.json());
@@ -16,7 +22,6 @@ app.use("/auth", authRouter);
 app.use("/profile", profileRouter);
 app.use("/request", requestRouter);
 app.use("/user", userRouter);
-
 
 connectDb()
     .then(
