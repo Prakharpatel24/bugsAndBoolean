81fb220c65cd4aee56387ef371c9c3cf6f3bb50b
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-05-27 11:36:46 +0530

    created_a_cronjob_for_sending_an_email

---
+++ utils/cronJob.js
@@ -0,0 +1,43 @@
+const cron = require('node-cron');
+const { subDays, startOfYesterday, endOfYesterday } = require("date-fns");
+const ConnectionRequest = require("../src/models/connectionRequest");
+const sendEmail = require("../utils/sendEmail");
+
+cron.schedule("0 8 * * *", async () => {
+    const yesterday = subDays(new Date, 1);
+    const yesterdayStart = startOfYesterday(yesterday);
+    const yesterdayEnd = endOfYesterday(yesterday);
+    const allPreviousDayPendingRequest = await ConnectionRequest.find({
+        status: "interested",
+        createdAt: {
+            $gte: yesterdayStart,
+            $lt: yesterdayEnd
+        }
+    }).populate("fromUserId toUserId");
+    const uniqueToUserEmailIds = [...new Set(allPreviousDayPendingRequest.map((request) => request?.toUserId?.emailId))];
+
+    for (emails of uniqueToUserEmailIds) {
+        const emailRes = await sendEmail.run(
+            "prakharpatel2001@gmail.com",
+            "noreply@bugsandboolean.com",
+            ` <html>
+                <body>
+                    <h2>Hello,</h2>
+                    <p>This is a gentle reminder that you have pending connection requests from yesterday.</p>
+                    <p> Don’t miss the opportunity to connect and collaborate!</p>
+                    <br />
+                    <p>Best regards,</p>
+                    <p>The Bugs&BooleanTeam</p>
+                </body>
+            </html>`,
+            `
+            Hello,
+            This is a gentle reminder that you have pending connection requests from yesterday.
+            on’t miss the opportunity to connect and collaborate!
+            Best regards,
+            The Bugs&Boolean Team
+            `,
+            "Follow up on your recent connection request"
+        );
+    }
+});
