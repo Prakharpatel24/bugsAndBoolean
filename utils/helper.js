f01b23ab1d7de24579a69226155cdba99dc527f3
Prakharpatel24 <prakharpatel2001@gmail.com>
2025-06-17 13:55:34 +0530

    created_helper_function_to_upload_file_to_s3

--- utils/helper.js
+++ utils/helper.js
@@ -1,7 +1,47 @@
 const validator = require('validator');
 
+const { Upload } = require("@aws-sdk/lib-storage");
+const { S3 } = require("@aws-sdk/client-s3");
+
 const checkForStrongPassword = (password) => {
     return validator.isStrongPassword(password);
 }
 
-module.exports ={checkForStrongPassword};
+const uploadFileToS3 = async (
+    fileBuffer,
+    bucketName,
+    key,
+    mimeType
+) => {
+    const s3 = new S3({
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY,
+            secretAccessKey: process.env.AWS_SECRET_KEY,
+        },
+
+        region: process.env.AWS_REGION,
+    });
+    console.log({
+        fileBuffer,
+    bucketName,
+    key,
+    mimeType
+    });
+    
+    const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: fileBuffer,
+        ContentType: mimeType,
+    }
+    return new Upload({
+        client: s3,
+        params,
+    }).done();
+}
+
+const getS3Url = (bucketName, region, key) => {
+    return `https://${bucketName}.s3.${region}.amazonaws.com/${key}`;
+}
+
+module.exports = { checkForStrongPassword, uploadFileToS3, getS3Url };
